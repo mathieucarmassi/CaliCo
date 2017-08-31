3def1be8fb3d1a7de5eb077055c23e281a1c6bb5
Mathieu Carmassi <mathieu.carmassi@gmail.com>
2017-08-31 18:19:50 +0200

    add the documentation of the first R6 class and add of a second model

--- R/models.R
+++ R/models.R
@@ -1,28 +1,88 @@
-model <- R6Class(classname = "model",
+#' A Reference Class to generates differents model objects
+#'
+#' @description See the function blabla which produces an instance of this class
+#'
+#' This class comes with a set of methods, some of them being useful for the user:
+#' See the documentation for blabla... Other methods
+#'  should not be called as they are designed to be used during the optimization process.
+#'
+#' Fields should not be changed or manipulated by the user as they are updated internally
+#' during the estimation process.
+#' @field code a function which takes in entry X and theta
+#' @field X the matrix of the forced variables
+#' @field theta the vector of parameters to estimate
+#' @field Yexp the experimental output
+#' @field n the number of experiments
+#' @field model the model choice see documentation
+#' @seealso The function ....
+gen <- R6Class(classname = "gen",
                  public = list(
-                   code  = NULL,
-                   X     = NULL,
-                   Yexp  = NULL,
-                   n     = NULL,
-                   initialize = function(code=NA,X=NA,Yexp=NA)
+                   code     = NULL,
+                   X        = NULL,
+                   Yexp     = NULL,
+                   n        = NULL,
+                   model    = NULL,
+                   initialize = function(code=NA,X=NA,Yexp=NA,model=NA)
                    {
                      self$code  <- code
                      self$X     <- X
                      self$Yexp  <- Yexp
                      self$n     <- length(Yexp)
+                     self$model <- model
+                     private$checkModels()
+                     private$selection()
                    }
                  ))
 
-model$set("public","model1",
+gen$set("private","checkModels",
+        function()
+        {
+          if (self$model != "model1" & self$model != "model2")
+          {
+            stop('Please elect a correct model')
+          }
+        })
+
+gen$set("public","model1",
           function(theta,sig2)
             {
             return(self$code(self$X,theta)+rnorm(self$n,0,sqrt(sig2)))
           })
 
+gen$set("public","model2",
+        function(theta,sig2,rho)
+        {
+          return(rho*self$code(self$X,theta)+rnorm(self$n,0,sqrt(sig2)))
+        })
+
+gen$set("private","selection",
+        function()
+          {
+          if (self$model== "model1")
+          {
+            funSelect <- self$model1
+          } else
+            if (self$model== "model2")
+          {
+            funSelect <- self$model2
+          }
+        })
+
+# Example of generation a model
+X <- cbind(runif(3),runif(3))
+code <- function(X,theta)
+{
+  return(X[,1]+theta*X[,2])
+}
+Yexp <- runif(3)
+test <- gen$new(code,X,Yexp,"model1")
+
+
 # To do in estim (rename to calib). Make initialize function of the elected model and the data to infer the estimation.
 # The user does not have to be to call the class model!!!
 
 estim <- R6Class(classname = "estim",
+                 inherit = gen,
                  public = list(
                    LSE = function(theta,obj=obj)
                    {
@@ -37,6 +97,12 @@ estim <- R6Class(classname = "estim",
 
 # The class predict uses an object estim and new data to predict a new Y
 
+calib <- function(code,X,Y)
+{
+  obj <- model$new(code,X,Y)
+  est <- estim$new()
+  return(est$opt(obj))
+}
 
 X <- cbind(runif(3),runif(3))
 code <- function(X,theta)
@@ -45,10 +111,5 @@ code <- function(X,theta)
 }
 Yexp <- runif(3)
 
-obj <- model$new(code,X,Yexp)
-test <- estim$new()
-test$LSE(c(1,1),obj=obj)
-test$opt(obj)
-
-
+calib(code,X,Yexp)
 
