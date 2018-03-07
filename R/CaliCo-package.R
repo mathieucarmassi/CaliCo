df1850e900cc36ca59ff868725c2ccd669b77655
Mathieu Carmassi <mathieu.carmassi@gmail.com>
2018-03-07 23:39:47 +0100

    modifs for cran

--- R/CaliCo-package.R
+++ R/CaliCo-package.R
@@ -5,7 +5,7 @@
 #'
 #' @useDynLib CaliCo
 #' @importFrom R6 R6Class
-#' @importFrom("stats", "rnorm")
+#' @importFrom stats rnorm
 #' @import ggplot2 DiceKriging DiceDesign FactoMineR coda parallel testthat MASS
 #'
 #' @details
