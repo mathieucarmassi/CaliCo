693b2aa267ab1a798bda76a7f84bee327f98a8a2
Mathieu Carmassi <mathieu.carmassi@edf.fr>
2018-04-18 14:32:20 +0200

    commit uncommitted changes

--- R/CaliCo-package.R
+++ R/CaliCo-package.R
@@ -6,7 +6,7 @@
 #' @useDynLib CaliCo
 #' @importFrom R6 R6Class
 #' @importFrom stats rnorm
-#' @import ggplot2 DiceKriging DiceDesign FactoMineR coda parallel testthat MASS
+#' @import ggplot2 DiceKriging DiceDesign coda parallel testthat MASS
 #'
 #' @details
 #' Package: CaliCo
